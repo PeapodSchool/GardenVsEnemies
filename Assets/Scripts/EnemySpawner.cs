7bd229660e6e4943b6a4b3acad5ead980898f2c3
unknown <ppmailstudy@gmail.com>
2021-05-14 17:46:42 +0300

    Add enemy spawner script and object, add enemy script.

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    [SerializeField] float minSpawnTime = 1f;
+    [SerializeField] float maxSpawnTime = 5f;
+    [SerializeField] Enemy[] enemyPrefabArray;
+    bool spawn = true;
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        while (spawn)
+        {
+            yield return new WaitForSeconds(
+                UnityEngine.Random.Range(minSpawnTime, maxSpawnTime));
+            SpawnAttackers();
+        }
+    }
+
+    private void SpawnAttackers()
+    {
+        int enemyIndex = Random.Range(0,
+            enemyPrefabArray.Length);
+        Spawn(enemyPrefabArray[enemyIndex]);
+    }
+
+    private void Spawn(Enemy enemy)
+    {
+        Enemy newEnemy = Instantiate(enemy,
+            transform.position, Quaternion.identity);
+    }
+
+    public void StopSpawn()
+    {
+        spawn = false;
+    }
+}
